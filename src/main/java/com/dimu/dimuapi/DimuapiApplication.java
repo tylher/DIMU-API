b8c14940c2e461c83476187d22a5be8a34714bb5
Damola <tylher123@gmail.com>
2025-01-04 03:11:15 +0100

    prepare project tools for dimu backend

---
+++ src/main/java/com/dimu/dimuapi/DimuapiApplication.java
@@ -0,0 +1,13 @@
+package com.dimu.dimuapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DimuapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DimuapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dimu/dimuapi/DimuapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dimu.dimuapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DimuapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
