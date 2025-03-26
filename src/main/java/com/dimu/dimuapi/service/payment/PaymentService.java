2e157bcbe4377b52e97e228b8125bfa90159a3a5
Damola <tylher123@gmail.com>
2025-03-26 10:38:23 +0100

    create payment setup

---
+++ src/main/java/com/dimu/dimuapi/service/payment/PaymentService.java
@@ -0,0 +1,5 @@
+package com.dimu.dimuapi.service.payment;
+
+public interface PaymentService {
+
+}
---
+++ src/main/java/com/dimu/dimuapi/service/payment/paystack/PaystackService.java
@@ -0,0 +1,6 @@
+package com.dimu.dimuapi.service.payment.paystack;
+
+import com.dimu.dimuapi.service.payment.PaymentService;
+
+public class PaystackService implements PaymentService {
+}
